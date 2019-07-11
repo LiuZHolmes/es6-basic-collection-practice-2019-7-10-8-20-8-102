bd14bcc17c25cf766a559a28f65831c2af436a70
Hendricks Liu <hendricks.liu@oocl.com>
2019-07-11 14:58:05 +0800

    feat:section-2/praactice-3.js

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
-}
+module.exports = collection => collection.reduce((result, element) => {
+  let ch = element.includes('-') ?  element.split('-')[0] : 
+    element.includes(':') ? element.split(':')[0] :
+    element.includes('[') ? element.split('[')[0]:
+    element;
+  let num = element.includes('-') ?  parseInt(element.split('-')[1]) : 
+    element.includes(':') ? parseInt(element.split(':')[1]) :
+    element.includes('[') ? parseInt(element.substr(element.search('\\\[') + 1,element.search('\\]')-element.search('\\\[')-1)):
+    1;
+  result.find(x => x.name === ch) ? 
+  result.find(x => x.name === ch).summary+=num 
+  : result.push({name:ch,summary:num});
+  return result;
+},[])
