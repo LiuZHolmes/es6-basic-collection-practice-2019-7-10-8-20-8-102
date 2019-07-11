e8fbeb3b23a76f3bbe5b31bcb29bb09076eb4dc2
Hendricks Liu <hendricks.liu@oocl.com>
2019-07-11 13:01:42 +0800

    feat:section-2/practice-1.js

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -1,5 +1,9 @@
 'use strict';
 
-module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
-}
+module.exports = collection => collection.reduce((result, element) => { 
+    result.find(x => x.key === element) ? 
+    result.find(x => x.key === element).count++ 
+    : result.push({key:element,count:1})
+    return result;
+  },[])
+
