7c162111e57e844be2e463ab5e83dc5c23e0a31d
Hendricks Liu <hendricks.liu@oocl.com>
2019-07-11 11:56:01 +0800

    feat: implement section-1/practice-4.js

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let result = [];
+  collectionA.forEach(x => {
+    if(objectB.value.find(y => x.key === y) != undefined) 
+      result.push(x.key);
+  });
+  return result;
 }
